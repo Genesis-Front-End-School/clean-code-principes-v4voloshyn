e4aea6524f9e24bab1b3098f32cbfcc524798164
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-04-26 22:26:28 +0300

    refactor: API structure by SOLID

---
+++ src/api/api.ts
@@ -0,0 +1,16 @@
+import { AxiosResponse } from 'axios';
+import { instance } from './instance';
+
+export const getRequest = async <T>(
+  endpoint: string,
+  params: Record<string, any> = {}
+): Promise<T> => {
+  const request: AxiosResponse<T> = await instance.get(endpoint, {
+    params,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return request.data;
+};
