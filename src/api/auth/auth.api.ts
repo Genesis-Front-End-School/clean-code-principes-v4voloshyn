a8dec7d8fe7e2b69739ac64b37c8bed5348bc4f7
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-08 12:17:49 +0300

    feat: auth.api test

--- src/api/auth/auth.api.ts
+++ src/api/auth/auth.api.ts
@@ -3,14 +3,30 @@ import axios, { AxiosResponse } from 'axios';
 import { BASE_URL } from '../constants';
 import { AUTH } from '../endpoints';
 
-type TokenResponse = {
+export type TokenResponse = {
   token: string;
 };
 
 export const getToken = async (): Promise<TokenResponse> => {
-  const response: AxiosResponse<TokenResponse> = await axios.get(
-    `${BASE_URL}${AUTH.TOKEN_ENDPOINT}`
-  );
+  try {
+    const response: AxiosResponse<TokenResponse> = await axios.get(
+      `${BASE_URL}${AUTH.TOKEN_ENDPOINT}`
+    );
 
-  return response.data;
+    if (!response.data) {
+      throw new Error('Empty response');
+    }
+
+    if (!response.data.token) {
+      throw new Error('Invalid response shape. Missing token');
+    }
+
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Failed to get token: ${error.message}`);
+    }
+
+    throw error;
+  }
 };
