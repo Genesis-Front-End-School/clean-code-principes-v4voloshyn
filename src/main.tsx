a736cb3b14132f824226b3b6a7179901b5ef7fc6
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-10 14:15:32 +0300

    warn‼️: rebuild folder structure into modules approach

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App';
+import { App } from './app/App';
 
-import './index.scss';
+import './shared/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
