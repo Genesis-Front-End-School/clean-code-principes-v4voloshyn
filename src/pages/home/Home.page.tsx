41c0af63b3141063e00e3f97290ad087ba4e9f91
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-10 18:59:20 +0300

    refactor: explicit definition of modules

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { CourseList } from '../../modules/course-list/CourseList';
+import { CourseList } from '../../modules/course-list/CourseList.module';
 
 export const Home: FC = () => {
   return <CourseList />;
