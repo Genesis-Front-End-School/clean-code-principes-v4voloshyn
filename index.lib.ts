253b4d90a8b0cbbcbfe6e17b6bdead7786adaf9c
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-21 22:47:23 +0300

    fix: package version and remove useThemeContext hook

--- index.lib.ts
+++ index.lib.ts
@@ -4,4 +4,3 @@ export * from './src/shared/UI/spinner/Spinner.component';
 export * from './src/shared/components/header/theme-switcher/Switch.component';
 export * from './src/shared/components/video-player/VideoPlayer.component';
 export * from './src/shared/hooks/useStorage';
-export * from './src/shared/hooks/useThemeContext';
